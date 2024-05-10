86582a0872d145ac11d34f7047e05e1409d83c98
Gabriel Matos Boubee <gm.boubee@gmail.com>
2024-05-10 21:38:40 +0000

    Release staging environment with 0 changes made by

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,14 +3,18 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_app from "./routes/_app.tsx";
-
+import * as $Hero from "./islands/Hero.tsx";
+import * as $SliderJSInfinite from "./islands/SliderJSInfinite.tsx";
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
     "./routes/_app.tsx": $_app,
   },
-  islands: {},
+  islands: {
+    "./islands/Hero.tsx": $Hero,
+    "./islands/SliderJSInfinite.tsx": $SliderJSInfinite,
+  },
   baseUrl: import.meta.url,
 } satisfies Manifest;
 
