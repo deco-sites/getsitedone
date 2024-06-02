e87f06db96119eb1d5300be98bf230ac4d830cce
gabrielMatosBoubee <gm.boubee@gmail.com>
2024-06-02 20:58:49 -0300

    feat: add image in Cards

--- sections/Content/Cards.tsx
+++ sections/Content/Cards.tsx
@@ -4,6 +4,8 @@ import Image from "apps/website/components/Image.tsx";
 /** @titleBy title */
 export interface Card {
   icon: ImageWidget;
+  image?: ImageWidget;
+  imageAlt?: ImageWidget;
   alt: string;
   title: string;
   /** @format html */
@@ -26,12 +28,17 @@ function Cards({ cards, title, backgroundColor }: Props) {
       <div class="container flex flex-col gap-20 py-16">
         {title && <div dangerouslySetInnerHTML={{ __html: title }} />}
         <div class="flex justify-between gap-8 items-center flex-wrap max-xl:justify-center">
-          {cards.map(({ alt, description, icon, title, width, height }) => (
+          {cards.map((
+            { alt, description, icon, image, imageAlt, title, width, height },
+          ) => (
             <div
               class="flex flex-col gap-6 border-2 border-[#C9CFCF] rounded-[40px] bg-white p-12 text-black"
               style={{ width, minHeight: height }}
             >
               <Image src={icon} alt={alt} width={40} height={40} />
+              {image && (
+                <Image src={image} alt={imageAlt} width={229} height={200} />
+              )}
               <p class="text-3xl font-bold">{title}</p>
               <div dangerouslySetInnerHTML={{ __html: description }} />
             </div>
