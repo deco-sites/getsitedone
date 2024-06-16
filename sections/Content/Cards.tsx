dc2b9fed8ed0b07ac2e293a02e266084cfa4ee16
João Victor Barbosa <joaovgb5@gmail.com>
2024-06-16 14:47:23 -0300

    hover adapt UI

--- sections/Content/Cards.tsx
+++ sections/Content/Cards.tsx
@@ -25,23 +25,22 @@ export interface Props {
 
 function Cards({ cards, title, backgroundColor, id }: Props) {
   return (
-    <div style={{ backgroundColor }} id={id}>
-      <div class="container flex flex-col gap-20 py-16">
+    <div style={{ backgroundColor }} id={id} className="p-4">
+      <div className="container mx-auto flex flex-col gap-20 py-16">
         {title && <div dangerouslySetInnerHTML={{ __html: title }} />}
-        <div class="flex gap-8 items-center flex-wrap justify-center">
-          {cards.map((
-            { alt, description, icon, image, imageAlt, title, width, height },
-          ) => (
+        <div className="flex flex-wrap gap-8 justify-center">
+          {cards.map(({ alt, description, icon, image, imageAlt, title, width, height }) => (
             <div
-              class="flex flex-col gap-6 border-2 border-[#C9CFCF] rounded-[40px] bg-white p-[32px] text-black"
+              key={title}
+              className="flex flex-col items-center gap-6 border-2 border-gray-300 rounded-2xl bg-white p-8 text-black transition-transform duration-300 ease-in-out transform hover:scale-105 hover:border-[#9900e5]"
               style={{ width, minHeight: height }}
             >
               <Image src={icon} alt={alt} width={40} height={40} />
               {image && (
                 <Image src={image} alt={imageAlt} width={266} height={200} />
               )}
-              <p class="text-3xl font-bold">{title}</p>
-              <div dangerouslySetInnerHTML={{ __html: description }} />
+              <p className="text-3xl font-bold text-center">{title}</p>
+              <div className="text-center" dangerouslySetInnerHTML={{ __html: description }} />
             </div>
           ))}
         </div>
