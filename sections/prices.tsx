658bc99100604b40a520948eed1490f0d37fb9a2
João Victor Barbosa <joaovgb5@gmail.com>
2024-06-17 19:38:20 -0300

    rigth cards

--- sections/prices.tsx
+++ sections/prices.tsx
@@ -8,7 +8,7 @@ export default function Section({ }: Props) {
   return (
     <div className="flex flex-col items-center py-20 bg-[#fafafa]">
       <h2 className="text-5xl font-extrabold mb-16 text-center text-gray-800">Serviços Desenvolvidos</h2>
-      <div className="grid grid-cols-1 lg:grid-cols-4 gap-12 max-w-7xl px-4">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 max-w-7xl px-2">
         {[
           {
             title: "Landing Page simples",
@@ -18,7 +18,7 @@ export default function Section({ }: Props) {
           {
             title: "Landing Page complexa",
             price: "R$999",
-            features: ["Landing page complexa", "3 páginas em um site único"]
+            features: ["Landing page complexa", "ou", "3 páginas em um site único"]
           },
           {
             title: "Site com integrações",
@@ -33,20 +33,26 @@ export default function Section({ }: Props) {
         ].map((service, index) => (
           <div
             key={index}
-            className="bg-white p-8 lg:p-10 rounded-2xl text-center border border-gray-300 shadow-lg hover:shadow-2xl transition-all duration-500 transform hover:-translate-y-2"
+            className="bg-white p-8 lg:p-10 rounded-2xl text-left border border-gray-300 shadow-lg hover:shadow-2xl transition-all duration-500 transform hover:-translate-y-2 w-full lg:max-w-xs"
           >
             <h3 className="text-xl lg:text-2xl font-semibold text-gray-900 mb-4">{service.title}</h3>
-            <p className="text-[#9900e5] text-2xl lg:text-3xl font-extrabold mb-6">{service.price}</p>
-            <ul className="text-left space-y-2 text-md lg:text-lg">
+            <p className="text-[#9900e5] text-2xl lg:text-3xl font-extrabold mb-5">{service.price}</p>
+            <ul className="space-y-2 text-xs lg:text-sm">
               {service.features.map((feature, idx) => (
                 <li key={idx} className="flex items-center text-gray-700">
-                  <Icon
-                    class="flex-shrink-0 text-[#b442ed] mr-2"
-                    id="circleCheck"
-                    height={20}
-                    width={20}
-                  />
-                  {feature}
+                  {feature !== "ou" ? (
+                    <>
+                      <Icon
+                        class="flex-shrink-0 text-[#b442ed] mr-2"
+                        id="circleCheck"
+                        height={20}
+                        width={20}
+                      />
+                      <span className="font-bold">{feature}</span>
+                    </>
+                  ) : (
+                    <span className="w-full text-center">{feature}</span>
+                  )}
                 </li>
               ))}
             </ul>
