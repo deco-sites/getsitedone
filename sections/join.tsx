7ccce736bb8eb323523cd5b89a79f19a5d3d9d72
João Victor <joao.victor@deco.cx>
2024-07-09 19:54:58 +0000

    Release staging environment with 11 changes made by Itamar Rocha Filho, João Victor

---
+++ sections/join.tsx
@@ -0,0 +1,24 @@
+interface Props {
+
+}
+
+export default function Section() {
+  return (
+    <div>
+  
+      <iframe
+        id="softr-5ecab925-9994-41b5-8983-f22223958eaf-user-accounts1"
+        src="https://experts.deco.cx/embed/pages/5ecab925-9994-41b5-8983-f22223958eaf/blocks/user-accounts1"
+        width="100%"
+        height="1000"
+        scrolling="no"
+        frameBorder="0"
+        style={{ border: "none" }}
+      ></iframe>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.2.11/iframeResizer.min.js"></script>
+      <script>
+        {`iFrameResize({ checkOrigin: false, log: true }, '#softr-5ecab925-9994-41b5-8983-f22223958eaf-user-accounts1');`}
+      </script>
+    </div>
+  );
+}
