34b053e84f5ef11839620db9645087b57b1953e3
joaovictorgb <joaovgb5@gmail.com>
2024-07-11 15:07:13 -0300

    upgrade finish

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -20,7 +20,7 @@ function Header({ title, navItens }: Props) {
   return (
     <header class="container h-[85px] py-6 flex justify-between items-center">
       <p
-        class="text-[25px] text-[#9900E5  ] font-bold"
+        class="text-[25px] text-[#9900E5] font-bold"
         style={clx(title.position)}
       >
         {title.text}
@@ -31,16 +31,34 @@ function Header({ title, navItens }: Props) {
           {navItens?.map(({ label, url }) => (
             <a class="text-sm font-medium text-[#616B6B]" href={url}>{label}</a>
           ))}
+          <a
+            class="bg-transparent hover:bg-[#9900E5] text-[#9900E5] font-semibold hover:text-white py-2 px-4 border border-[#9900E5] hover:border-transparent rounded-full"
+            href="/join"
+            style={{ padding: '10px 20px', borderRadius: '25px' }}
+          >
+            Sign Up for Makers
+          </a>
         </div>
       )}
       {/** MOBILE */}
-      {navItens && <Drawer>
-        <ul class="flex flex-col text-[#9900E5] bg-white gap-4 h-full px-4 py-4 w-xl">{navItens.map(({ label, url }) =>
-          <a href={url}>
-            <li>{label}</li>
-          </a>)}
-        </ul>
-      </Drawer>}
+      {navItens && (
+        <Drawer>
+          <ul class="flex flex-col text-[#9900E5] bg-white gap-4 h-full px-4 py-4 w-xl">
+            {navItens.map(({ label, url }) => (
+              <a href={url}>
+                <li>{label}</li>
+              </a>
+            ))}
+            <a
+              class="bg-transparent hover:bg-[#9900E5] text-[#9900E5] font-semibold hover:text-white py-2 px-4 border border-[#9900E5] hover:border-transparent rounded-full"
+              href="/join"
+              style={{ padding: '10px 20px', borderRadius: '25px' }}
+            >
+              Sign Up for Makers
+            </a>
+          </ul>
+        </Drawer>
+      )}
     </header>
   );
 }
