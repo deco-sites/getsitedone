eaf844fc7c01799fb658acb6e65e749726443bbb
joaovictorgb <joaovgb5@gmail.com>
2024-07-12 11:53:58 -0300

    prod button

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -62,7 +62,7 @@ function Header({ title, navItens, currentPath }: Props) {
                 class="bg-transparent hover:bg-[#9900E5] text-[#9900E5] font-semibold hover:text-white py-2 px-4 border border-[#9900E5] hover:border-transparent rounded-full px-5 py-2.5"
                 href="/join"
               >
-                Sign Up for Makers
+                Inscreva-se para criadores  
               </a>
             </li>
             <li>
