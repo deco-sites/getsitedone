6e0bc2ef9ff2f022658016033b97b263a18acc01
joaovictorgb <joaovgb5@gmail.com>
2024-08-19 12:57:04 -0300

    publish withpopup

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -12,19 +12,20 @@ import * as $$$$$$3 from "./sections/Content/Cta.tsx";
 import * as $$$$$$4 from "./sections/Content/Faq.tsx";
 import * as $$$$$$5 from "./sections/Content/Hero.tsx";
 import * as $$$$$$6 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$15 from "./sections/decoads.tsx";
+import * as $$$$$$16 from "./sections/decoads.tsx";
 import * as $$$$$$7 from "./sections/Footer.tsx";
 import * as $$$$$$8 from "./sections/Footer/Footer.tsx";
 import * as $$$$$$9 from "./sections/Header/Header.tsx";
 import * as $$$$$$10 from "./sections/HeaderwithButton.tsx";
-import * as $$$$$$11 from "./sections/HomeUS.tsx";
-import * as $$$$$$12 from "./sections/ItWorks.tsx";
-import * as $$$$$$16 from "./sections/join.tsx";
-import * as $$$$$$17 from "./sections/joinen.tsx";
-import * as $$$$$$18 from "./sections/prices.tsx";
-import * as $$$$$$13 from "./sections/PricesUS.tsx";
-import * as $$$$$$19 from "./sections/signup.tsx";
-import * as $$$$$$14 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$11 from "./sections/Headerwithpopup.tsx";
+import * as $$$$$$12 from "./sections/HomeUS.tsx";
+import * as $$$$$$13 from "./sections/ItWorks.tsx";
+import * as $$$$$$17 from "./sections/join.tsx";
+import * as $$$$$$18 from "./sections/joinen.tsx";
+import * as $$$$$$19 from "./sections/prices.tsx";
+import * as $$$$$$14 from "./sections/PricesUS.tsx";
+import * as $$$$$$20 from "./sections/signup.tsx";
+import * as $$$$$$15 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
@@ -35,19 +36,20 @@ const manifest = {
     "site/sections/Content/Faq.tsx": $$$$$$4,
     "site/sections/Content/Hero.tsx": $$$$$$5,
     "site/sections/Content/Testimonials.tsx": $$$$$$6,
-    "site/sections/decoads.tsx": $$$$$$15,
+    "site/sections/decoads.tsx": $$$$$$16,
     "site/sections/Footer.tsx": $$$$$$7,
     "site/sections/Footer/Footer.tsx": $$$$$$8,
     "site/sections/Header/Header.tsx": $$$$$$9,
     "site/sections/HeaderwithButton.tsx": $$$$$$10,
-    "site/sections/HomeUS.tsx": $$$$$$11,
-    "site/sections/ItWorks.tsx": $$$$$$12,
-    "site/sections/join.tsx": $$$$$$16,
-    "site/sections/joinen.tsx": $$$$$$17,
-    "site/sections/prices.tsx": $$$$$$18,
-    "site/sections/PricesUS.tsx": $$$$$$13,
-    "site/sections/signup.tsx": $$$$$$19,
-    "site/sections/Theme/Theme.tsx": $$$$$$14,
+    "site/sections/Headerwithpopup.tsx": $$$$$$11,
+    "site/sections/HomeUS.tsx": $$$$$$12,
+    "site/sections/ItWorks.tsx": $$$$$$13,
+    "site/sections/join.tsx": $$$$$$17,
+    "site/sections/joinen.tsx": $$$$$$18,
+    "site/sections/prices.tsx": $$$$$$19,
+    "site/sections/PricesUS.tsx": $$$$$$14,
+    "site/sections/signup.tsx": $$$$$$20,
+    "site/sections/Theme/Theme.tsx": $$$$$$15,
   },
   "apps": {
     "site/apps/deco/analytics.ts": $$$$$$$$$$$0,
---
+++ sections/Headerwithpopup.tsx
@@ -0,0 +1,198 @@
+import { useEffect, useRef } from "preact/hooks";
+import { useSignal } from "@preact/signals";
+import Slider from "../components/ui/Slider.tsx";
+import { useId } from "../sdk/useId.ts";
+import SliderJSInfinite from "../islands/SliderJSInfinite.tsx";
+import Icon from "../components/ui/Icon.tsx";
+import Image from "apps/website/components/Image.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
+import { JSX } from "preact";
+
+export interface Carousel {
+  text: string;
+}
+
+export interface Props {
+  /** @format html */
+  text?: string;
+  singleText?: string;
+  placeholder?: string;
+  number: string;
+  defaultMessage?: string;
+  buttonText: string;
+  buttonImage?: string;
+  backgroundImage?: ImageWidget;
+  backgroundColor?: string;
+  carousel?: Carousel[];
+  id?: string;
+}
+
+const HeroForm = ({
+  placeholder,
+  buttonText,
+  number,
+  defaultMessage,
+  buttonImage,
+  inputRef,
+}: {
+  placeholder?: string;
+  buttonText: string;
+  number: string;
+  defaultMessage?: string;
+  buttonImage?: string;
+  inputRef: React.RefObject<HTMLInputElement>;
+}) => {
+  const handleSubmit = (event: JSX.TargetedEvent<HTMLFormElement, Event>) => {
+    event.preventDefault();
+    const inputValue = inputRef.current?.value || "";
+    const whatsText = `${defaultMessage || ""}${inputValue}`;
+    globalThis.open(`https://wa.me/${number}?text=${whatsText}`, "_blank");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      class="w-full flex items-center justify-center relative"
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder={placeholder}
+        class="hidden"
+        aria-label={placeholder}
+      />
+      <div class="relative flex justify-center items-center">
+        <div class="absolute inset-0 bg-purple-400 rounded-full blur-lg opacity-60 z-0">
+        </div>
+        <button
+          class="relative text-purple-600 bg-white border border-purple-400 rounded-full shadow-xl flex justify-center items-center text-lg md:text-xl font-inter px-6 md:px-8 py-3 md:py-4 transition-transform duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 z-10"
+          type="submit"
+          aria-label={buttonText}
+        >
+          {buttonImage && (
+            <img
+              src={buttonImage}
+              alt="Icon"
+              class="mr-2 h-6 w-6 md:h-8 md:w-8 z-10"
+              loading="lazy"
+            />
+          )}
+          <span class="flex items-center z-10 text-current">{buttonText}</span>
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const HeroCarousel = ({
+  carousel,
+  id,
+}: {
+  carousel?: Carousel[];
+  id: string;
+}) => (
+  <div class="w-full relative mt-[-20px] flex justify-center" id={id}>
+    <div class="absolute inset-y-0 left-0 w-16 bg-gradient-to-r from-white to-transparent pointer-events-none">
+    </div>
+    <div class="absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-white to-transparent pointer-events-none">
+    </div>
+    <Slider class="carousel carousel-start flex items-center gap-2 md:gap-4 px-4 md:px-0">
+      {carousel?.map(({ text }, index) => (
+        <Slider.Item key={index} class="carousel-item" index={index}>
+          <div class="flex items-center gap-2 p-4 bg-transparent rounded-md w-full">
+            <Icon
+              class="flex-shrink-0 text-[#b442ed]"
+              id="circleCheck"
+              height={15}
+              width={15}
+            />
+            <p class="whitespace-nowrap text-base md:text-lg text-gray-900 w-full text-center">
+              {text}
+            </p>
+          </div>
+        </Slider.Item>
+      ))}
+    </Slider>
+
+    <SliderJSInfinite rootId={id} interval={2000} isPerItem />
+  </div>
+);
+
+const HerowithButton = ({
+  placeholder,
+  buttonText,
+  text,
+  singleText,
+  carousel,
+  backgroundImage,
+  buttonImage,
+  number,
+  defaultMessage,
+  backgroundColor,
+  id: sectionId,
+}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const id = useId();
+
+  const isMobile = useSignal(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isCurrentlyMobile = globalThis.innerWidth <= 768;
+      if (isCurrentlyMobile !== isMobile.value) {
+        isMobile.value = isCurrentlyMobile;
+      }
+    };
+
+    handleResize(); // Executa na montagem
+    globalThis.addEventListener("resize", handleResize);
+
+    return () => globalThis.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div
+      class={`w-full min-h-screen flex flex-col justify-center items-center text-center px-4 sm:px-6 md:px-8 lg:px-12 relative ${
+        backgroundColor ? `bg-[${backgroundColor}]` : ""
+      }`}
+      id={sectionId}
+    >
+      {backgroundImage && (
+        <Image
+          class="absolute -z-10 w-full h-full object-cover opacity-120"
+          src={backgroundImage}
+          width={970}
+          height={451.44}
+          loading="lazy"
+        />
+      )}
+      <div class="container flex flex-col justify-center items-center py-8 gap-6 md:gap-8 lg:gap-10 w-full md:w-11/12 lg:w-8/12 relative">
+      { text && (
+  <div
+    class="w-full text-center text-[#0D1717] break-words text-[50px] sm:text-[80px] leading-[60px] sm:leading-[90px] font-[500] mt-[-90px] font-albert sans"
+    dangerouslySetInnerHTML={{ __html: text }}
+  />
+)}
+
+        {singleText && (
+          <div
+            class="w-full text-center text-gray-700 mt-[-10px] text-[18px] md:text-[28px] font-[500] font-albertsans"
+            dangerouslySetInnerHTML={{ __html: singleText }}
+          />
+        )}
+
+        <HeroForm
+          placeholder={placeholder}
+          buttonText={buttonText}
+          number={number}
+          defaultMessage={defaultMessage}
+          buttonImage={buttonImage}
+          inputRef={inputRef}
+        />
+        {carousel && <HeroCarousel carousel={carousel} id={id} />}
+      </div>
+    </div>
+  );
+};
+
+export default HerowithButton;
